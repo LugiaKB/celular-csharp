67f38c709706bdcb78bdb6ffe55c0836e3d3be4b
Iury Mikael <mikaeliury@gmail.com>
2024-01-09 08:30:13 -0300

    feat: implementadas propriedades na classe nokia

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
+Iphone i = new Iphone("40028922", "plus pro 14", "123456", "5 gb");
+
+i.Ligar();
+i.ReceberLigacao();
+i.InstalarAplicativo("Safari");
+
+Nokia n = new Nokia("4002", "lumia", "1234567", "2 gb");
+
+n.Ligar();
+n.ReceberLigacao();
+n.InstalarAplicativo("Whatsapp");
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
